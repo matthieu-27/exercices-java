5c370d2a40e857e1a4880b6febcf4610ace232b8
matthieu-27 <matthieu27@pm.me>
2025-10-28 12:53:47 +0100

    Commit de l'exercice 3

---
+++ BaseEx3Loop/src/BaseEx3Loop.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class BaseEx3Loop {
+	public static void main(String[] args) {
+		int userNumber = 0;
+		
+		if(args.length <= 0){
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Entrez un nombre entier :");
+			
+			while(!sc.hasNextInt()){
+				sc = new Scanner(System.in);
+				System.out.println("Veuillez entrez un nombre entier");
+			}
+			
+			userNumber = sc.nextInt();
+			sc.close();
+			System.out.println(getResult(userNumber));
+
+		} else {
+			for(String arg : args) {
+				Scanner argScan = new Scanner(arg);
+				if(argScan.hasNextInt()) {
+					System.out.println(getResult(Integer.parseInt(arg)));
+				}
+				argScan.close(); 
+			}
+		}
+	}
+	
+	public static String getResult(int number) {
+		
+		String str = number + " ";
+
+		if(number >= 0) {
+			str += "est positif ";
+		}else {
+			str += "est negatif ";
+		}
+		
+		if(number % 2 == 0) {
+			str += "et pair.";
+		}else {
+			str += "et impair.";
+		}
+		
+		return str;
+	}
+
+}
