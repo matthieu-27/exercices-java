59389f28885e0d67c19c2cb366f185aa4259ddd8
matthieu-27 <matthieu27@pm.me>
2025-10-29 13:17:07 +0100

    Ajout de la fonction buildTriangle

--- BaseEx6Operation/src/Main.java
+++ BaseEx6Operation/src/Main.java
@@ -6,5 +6,17 @@ public class Main {
 		System.out.println(Operation.mul(5, 2));
 		System.out.println(Operation.div(5, 2));
 		System.out.println(Operation.div(5, 0));
+		buildTriangle(6);
+	}
+	
+	public static void buildTriangle(int size) {		 
+		for (int k = 0; k <= size-1; k++)
+		{
+			for (int j = 0; j < size-k; j++)
+				System.out.print(" ");
+			for (int i = 0; i < (k*2+1); i++)
+				System.out.print("*");
+			System.out.println("");
+		}
 	}
 }
