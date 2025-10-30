626d256afb9d0f966ca9a3779cf40587b549dc99
matthieu-27 <matthieu27@pm.me>
2025-10-30 08:28:16 +0100

    Ajout des commentaires

--- BaseEx3Loop/src/BaseEx3Loop.java
+++ BaseEx3Loop/src/BaseEx3Loop.java
@@ -2,34 +2,48 @@ import java.util.Scanner;
 
 public class BaseEx3Loop {
 	public static void main(String[] args) {
-		int userNumber = 0;
 		
+		// Here we check arguments presence
 		if(args.length == 0){
 			
+			// asks the user to enter an integer
 			Scanner scan = new Scanner(System.in);
 			System.out.println("Entrez un nombre entier :");
 			
+			
+			// loop until the scan can find a valid integer
 			while(!scan.hasNextInt()){
-				System.out.println("Veuillez entrez un nombre entier :");
-				scan.next();
+				System.out.println("Veuillez entrez un nombre entier :"); // we ask again
+				scan.next(); // and clear invalid input
 			}
 			
-			userNumber = scan.nextInt();
+			// assign valid integer to userNumber
+			int userNumber = scan.nextInt();
+			System.out.println(checkNumber(userNumber)); // prints out the result
+			
+			// the scanner can be closed
 			scan.close();
-			System.out.println(getResult(userNumber));
-
 		} else {
 			for(String arg : args) {
+				// Create a new scanner for each arguments
 				Scanner argScan = new Scanner(arg);
+				// If the argument is a valid integer
 				if(argScan.hasNextInt()) {
-					System.out.println(getResult(Integer.parseInt(arg)));
+					System.out.println(checkNumber(Integer.parseInt(arg))); // prints out the result
 				}
+				// closes scanner to prevent memory leak
 				argScan.close(); 
 			}
 		}
 	}
 	
-	public static String getResult(int number) {
+	/*
+	 * This method checks if a number is positive/negative and even/odd
+	 * 
+	 * @param number The number to be checked
+	 * @return Returns a string saying if positive/negative and even/odd
+	 */
+	public static String checkNumber(int number) {
 		String str = number + " ";
 
 		if(number >= 0) str += "est positif ";
