d6048ebb58fd96ee6b8e9f2cc3bdc0a63834e88a
matthieu-27 <matthieu27@pm.me>
2025-10-30 14:17:55 +0100

    Ajout de la fonction de calcul des deux plus gros nombre d'un tableau

--- BaseEx6Operation/src/Main.java
+++ BaseEx6Operation/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Main {
+	
+	// 6.1
+	
 	public static void main(String[] args) {
 		System.out.println(Operation.add(5, 2));
 		System.out.println(Operation.sub(5, 2));
@@ -8,7 +15,11 @@ public class Main {
 		System.out.println(Operation.div(5, 0)); // division by zero returns dividend
 		buildTriangle(6);
 		makeTriangle(6);
+		int[] intTable = { 78, 6, -250, 2, 12, 9};
+		System.out.printf("Résultat de l'addition des deux plus grand du tableau: %d", sumBigestNumbers(intTable));
 	}
+	
+	// 6.2
 	
 	public static void buildTriangle(int size) {		 
 		for (int k = 0; k <= size-1; k++)
@@ -33,5 +44,25 @@ public class Main {
 		for (int i = 0; i < size; i++) {
 			System.out.print(c); // adds char to line
 		}
+	}
+	
+	// 6.3
+	
+	/*
+	 * Returns sum of two biggest numbers in array
+	 * 
+	 * @param array a primitive integer array
+	 * @return sum of max number and next closest to him
+	 */
+	public static int sumBigestNumbers(int[] arr) {
+	    ArrayList<Integer> integerArray =
+	            (ArrayList<Integer>) Arrays.stream(arr).boxed().collect(Collectors.toList()); // convert primitive array to array list
+	    int max = Arrays.stream(arr).max().getAsInt(), secMax = 0; // get max value using Arrays utility class
+	    int indexOfMax = integerArray.indexOf(max); // get index of max value
+	    integerArray.remove(indexOfMax); // remove max value from array list
+	    for(Integer i : integerArray) { // loop and find max value
+	    	if(i > secMax) secMax = i;
+	    }
+	    return (int) Operation.add(max, secMax); // return casted result, because why not.
 	}
 }
