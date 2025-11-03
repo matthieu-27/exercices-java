1df6dd82b16491f85723f8e272b23d0c22ae50a2
matthieu-27 <matthieu27@pm.me>
2025-11-03 15:29:17 +0100

    Fix: removed useless condition on while, used it to close scan and break, renamed lettres to letters

--- HangedMan/src/Main.java
+++ HangedMan/src/Main.java
@@ -16,39 +16,40 @@ public class Main {
         boolean finishedGame = false;
 
         Scanner scanner = new Scanner(System.in);
-        while (errors < maxErrors || !finishedGame) {
+        while (errors < maxErrors) {
         	
             // prints out current state
             System.out.println("Mot : " + new String(displayArray));
             System.out.println("Erreurs : " + errors + "/" + maxErrors);
             System.out.print("Proposez une lettre : ");
-            char lettre = scanner.next().charAt(0);
+            char letter = scanner.next().charAt(0);
 
 
             // update game state
-            if (wordToGuess.indexOf(lettre) >= 0) {
+            if (wordToGuess.indexOf(letter) >= 0) {
                 // found letter !
                 for (int i = 0; i < wordToGuess.length(); i++) {
-                    if (wordToGuess.charAt(i) == lettre) {
-                        displayArray[i] = lettre;
+                    if (wordToGuess.charAt(i) == letter) {
+                        displayArray[i] = letter;
                     }
                 }
-                foundLetters.add(lettre);
+                foundLetters.add(letter);
             } else {
                 // not found !
                 errors++;
             }
-
             // Check if word is guessed
             if (new String(displayArray).equals(wordToGuess)) {
                 System.out.println("Félicitations, vous avez gagné ! Le mot était : " + wordToGuess);
                 finishedGame = true;
-                break;
             }
             else if (errors >= maxErrors) {
                 System.out.println("Désolé, vous avez perdu. Le mot était : " + wordToGuess);
                 finishedGame = true;
-                break;
+            }
+            if(finishedGame) {
+            	scanner.close();
+            	break;
             }
 
         }
