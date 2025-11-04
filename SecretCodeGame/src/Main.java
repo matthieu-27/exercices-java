f8933b1964af841ec8e1967b81b998de935a68e5
matthieu-27 <matthieu27@pm.me>
2025-11-04 13:25:44 +0100

    retrait condition jamais vraie, amélioration des compteurs

--- SecretCodeGame/src/Main.java
+++ SecretCodeGame/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,49 +13,57 @@ public class Main {
 		
 		// Initialisation du scanner
 		Scanner userScan = new Scanner(System.in);
+		char[] displayCode = new char[codeArray.length];
+
+        Arrays.fill(displayCode, '_'); // fills array with underscores
+
 		
-		System.out.println("Code tiré: " + formattedCode);
+		System.out.println("Code tiré: " + new String(displayCode));
 		int tries = 0;
 		String[] userArray = new String[codeArray.length]; 
-		System.out.println(tries == 0 ? "Veuillez entrer un code :" : "");
-		boolean gameFinished = false;
+		System.out.println("Veuillez entrer un code :");
 
-		while(userScan.hasNext() && !gameFinished) {
-			System.out.println(tries == 0 ? "": "Veuillez entrer un code :");
-
+		while(userScan.hasNext()) {
+			System.out.println("Veuillez entrer un code :");
+			++tries;
 			if(userScan.hasNextInt()) {
 				String userInput = userScan.nextLine();
+				if(userInput.length() != 4) {
+					System.out.println("!! Veuillez entrer un code à 4 chiffres !!");
+					continue;
+				}
 				userArray = userInput.split("");
 			} else {
 				System.out.println("Veuillez entrer un code valide.");
 				userScan.next();
+				continue;
 			}
 			
 			int goodAndPlaced = 0, goodAndNotPlaced = 0;
 			
 			for (int i = 0; i < codeArray.length; i++) {
-				if(!codeArray[i].equals(userArray[i])) {
-					if(codeArray[i].equals(userArray[i])) {
-						goodAndPlaced++;
+					if(codeArray[i].equals(userArray[i])) goodAndPlaced++;
+					else {
+						for (int j = 0; j < codeArray.length; j++) {
+							if(codeArray[i].equals(userArray[j])) goodAndNotPlaced++;
+						}						
 					}
-					for (int j = 0; j < codeArray.length; j++) {
-						if(codeArray[i].equals(userArray[j])) {
-							goodAndNotPlaced++;
-						}
-					}
-				} else {
-					System.out.println("Gagné ! gg. en " + (tries == 0 ? "1" : tries) + " coups");
-					gameFinished = true;
-					System.exit(1);
-				}
+			} 
+			
+			if(goodAndPlaced == codeArray.length) {
+				System.out.println("Gagné ! gg. en " + (tries + 1) + " coups");
+				break;
 			}
-			++tries;
+			
 			System.out.print(tries);
 			System.out.print(tries == 1 ? "er " : "ème ");
 			System.out.println("éssai:");
 			System.out.println("Bon et bien placé: " + goodAndPlaced);
-			System.out.println("Bon et mal placé: " + (goodAndNotPlaced - goodAndPlaced));
+			System.out.println("Bon et mal placé: " + goodAndNotPlaced);
+
 		}
+		
+			
 	}
-	
 }
+	
