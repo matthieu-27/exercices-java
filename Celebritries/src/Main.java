95cb8c0f7fec5523c15c2dd38cd5c214767f1477
matthieu-27 <matthieu27@pm.me>
2025-11-04 09:46:51 +0100

    Amélioration de la structure des données

--- Celebritries/src/Main.java
+++ Celebritries/src/Main.java
@@ -9,27 +9,25 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Map<String, int[]> peopleList = new HashMap<String, int[]>();
-
-		peopleList.put("Albert", new int[]{2, 5, 6});
-		peopleList.put("Bénédicte", new int[]{3, 5, 6});
-		peopleList.put("Christophe", new int[]{2, 4, 5, 6});
-		peopleList.put("Delphine", new int[]{1, 2, 5, 6, 8});
-		peopleList.put("Edouard", new int[]{6});
-		peopleList.put("Françoise", new int[]{5});
-		peopleList.put("Gaston", new int[] {2, 3, 5, 6, 8});
-		peopleList.put("Heloïse", new int[] {2, 4, 5, 6, 7});
+		Map<String, Set<Integer>> peopleList = new HashMap<String, Set<Integer>>();
 		
-		List<String> intToStrList = new ArrayList<>(peopleList.size());
+		List<String> potentialStars  = new ArrayList<String>();
+
+		peopleList.put("Albert", new HashSet<>(Arrays.asList(2, 5, 6)));
+		
+		peopleList.put("Bénédicte", new HashSet<>(Arrays.asList(3, 5, 6)));
+		peopleList.put("Christophe", new HashSet<>(Arrays.asList(2, 4, 5, 6)));
+		peopleList.put("Delphine", new HashSet<>(Arrays.asList(1, 2, 5, 6, 8)));
+		peopleList.put("Edouard", new HashSet<>(Arrays.asList(6)));
+		peopleList.put("Françoise", new HashSet<>(Arrays.asList(5)));
+		peopleList.put("Gaston", new HashSet<>(Arrays.asList(2, 3, 5, 6, 8)));
+		peopleList.put("Heloïse", new HashSet<>(Arrays.asList(2, 4, 5, 6, 7)));
+		
 
 		// start looping on peopleList
-		for(Map.Entry<String, int[]> people : peopleList.entrySet()) {
-			for (Integer myInt : people.getValue()) { 
-				  intToStrList.add(String.valueOf(myInt)); 
-			}
+		for(Map.Entry<String, Set<Integer>> people : peopleList.entrySet()) {
 
-			System.out.println(people.getKey() + intToStrList);
-			intToStrList.clear();
+			System.out.println(people.getKey() + " -> " + people.getValue());
 		}
 	}
 }
