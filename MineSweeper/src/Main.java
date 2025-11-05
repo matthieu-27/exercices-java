07b39f41fe0094aaf0d2020b4617e93de6e5baf9
matthieu-27 <matthieu27@pm.me>
2025-11-05 11:38:44 +0100

    Fix: colonnes et lignes buguées

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public class Main {
     private static final int BOMBS = 9;
-    private static final int[] ROWS_HEAD = {1, 2, 3, 4, 5, 6};
-    private static final String[] COLS_HEAD = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+    private static final int[] ROWS_HEAD = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private static final String[] COLS_HEAD = {"A", "B", "C", "D", "E", "F"};
     private static final List<String> gridCells = new ArrayList<>();
     private static final Map<String, Integer> grid = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         // create grid
-        for (int i = 0; i < COLS_HEAD.length; i++) {
-            for (int j = 0; j < ROWS_HEAD.length; j++) {
-                String cellName = COLS_HEAD[i] + ROWS_HEAD[j];
+        for (int i = 0; i < ROWS_HEAD.length; i++) {
+            for (int j = 0; j < COLS_HEAD.length; j++) {
+                String cellName = COLS_HEAD[j] + ROWS_HEAD[i];
                 gridCells.add(cellName);
                 grid.put(cellName, 0);
             }
