0ee91e31c2cd37fcf7e8478f065f6ae0af7d9464
matthieu-27 <matthieu27@pm.me>
2025-11-12 15:16:34 +0100

    First commit: project setup

---
+++ PooEx3Heritage/src/fr/fms/entities/City.java
@@ -0,0 +1,83 @@
+package fr.fms.entities;
+
+public class City {
+    private String name;
+    private String country;
+    private int population;
+    public static int instanceNumber = 0;
+
+    public City(String name, String country, int population){
+        this.name = name;
+        this.country = country;
+        this.population = checkPopulation(population);
+        instanceNumber++;
+    }
+
+    public City(String name, String country){
+        this.name = name;
+        this.country = country;
+        this.population = 0;
+        instanceNumber++;
+    }
+
+    public City(String name, int population){
+        this.name = name;
+        this.country = "unknown";
+        this.population = checkPopulation(population);
+        instanceNumber++;
+    }
+
+    private static int checkPopulation(int population){
+        // ici ont s'assure que la population de la ville n'est pas négative.
+        try {
+            if(population < 0){
+                throw new RuntimeException("La population ne peux pas être négative");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = checkPopulation(population);
+    }
+
+    public void display(){
+        System.out.println("Ville de " + this.getName() +" en " + this.getCountry() + " ayant: " + this.getPopulation() + " habitants.");
+    }
+
+    public String personDisplay() {
+        return "Ville de naissance: " + this.getName() + ", " + this.getCountry();
+    }
+
+
+    @Override
+    public String toString() {
+        return "[Ville:" + this.getName() + "] " + "[Pays:" + this.getCountry() + "] " +"[Population: " + this.getPopulation() + "]";
+    }
+
+    public static int getInstanceNumber() {
+        return instanceNumber;
+    }
+}
---
+++ PooEx3Heritage/src/fr/fms/entities/Person.java
@@ -0,0 +1,105 @@
+package fr.fms.entities;
+import java.util.ArrayList;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+    private City bornCity;
+    public static ArrayList<Person> personList = new ArrayList<>();
+
+    public Person(String firstName, String lastName, int age, String address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+        this.bornCity = null;
+        personList.add(this);
+    }
+
+    public Person(String firstName, String lastName, int age, String address, City bornCity){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+        this.bornCity = bornCity;
+        personList.add(this);
+    }
+
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = "unknown";
+        this.bornCity = null;
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = 200;
+        this.address = "unknown";
+        this.bornCity = null;
+    }
+
+    public static ArrayList<Person> getPersonList() {
+        return personList;
+    }
+
+    public static ArrayList<Person> fromParis(ArrayList<Person> personList){
+        ArrayList<Person> result = new ArrayList<>();
+        for(Person person: personList){
+            if(person.getAddress().equalsIgnoreCase("paris") || person.getBornCity().getCountry().equalsIgnoreCase("france")){
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public City getBornCity() {
+        return bornCity;
+    }
+
+    public void setBornCity(City bornCity) {
+        this.bornCity = bornCity;
+    }
+
+    @Override
+    public String toString(){
+        String city = this.getBornCity() != null ? this.getBornCity().personDisplay() : "null";
+        return ("Person " + "[firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ", age=" + this.getAge() + ", address=" + this.getAddress() +"]" + city);
+    }
+}
