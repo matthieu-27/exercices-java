7e583487bd5ffe36d762dba3af653b40c6b1e4db
matthieu-27 <matthieu27@pm.me>
2025-11-13 09:46:10 +0100

    Exercice 3.3: controle des données saisies

--- PooEx3Heritage/src/fr/fms/entities/SalesMan.java
+++ PooEx3Heritage/src/fr/fms/entities/SalesMan.java
@@ -1,13 +1,27 @@
 package fr.fms.entities;
 
 public class SalesMan extends Person{
+    private static double MIN_PERCENTAGE = 3.5;
     private double percentageSalary;
     private String workPlace;
 
     public SalesMan(String firstName, String lastName, int age, String address, City bornCity, double percentageSalary, String workPlace){
         super(firstName, lastName, age, address, bornCity);
-        this.percentageSalary = percentageSalary;
+        this.percentageSalary = checkSalary(percentageSalary);
         this.workPlace = workPlace;
+    }
+
+    private static double checkSalary(double salary){
+        // ici ont s'assure que le pourcentage du salaire n'est pas négatif.
+        try {
+            if(salary < 0){
+                throw new RuntimeException("Le pourcentage du salaire de peux être négatif");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return MIN_PERCENTAGE;
+        }
+        return salary;
     }
 
     public double getPercentageSalary() {
@@ -15,7 +29,7 @@ public class SalesMan extends Person{
     }
 
     public void setPercentageSalary(double percentageSalary) {
-        this.percentageSalary = percentageSalary;
+        this.percentageSalary = checkSalary(percentageSalary);
     }
 
     public String getWorkPlace() {
--- PooEx3Heritage/src/fr/fms/entities/TestEmployee.java
+++ PooEx3Heritage/src/fr/fms/entities/TestEmployee.java
@@ -5,7 +5,7 @@ public class TestEmployee {
         Employee bill = new Employee("Bill", "Gates", 65, "habitant au US", new Capital("Washington", "USA", 1000000, "W-M"), "Entreprise B&M", 100000);
         Employee musk = new Employee("Elon", "Musk", 53, "habitant a Palo Alto", new City("Pretoria", "Afrique du Sud", 800000), "Tesla", 150000);
         SalesMan robert = new SalesMan("Robert", "Pires", 19, "rue des muscles", new City("Paris", "France", 2000000), 5.2, "Ailier Gauche");
-
+        robert.setPercentageSalary(-3.5);
 
         System.out.println(bill);
         System.out.println(musk);
